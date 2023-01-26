78f4b1bb756163d8b8d0f4d10ab5897b131914c8
Alex-Krysztofiak-lasalle <114675157+Alex-Krysztofiak-lasalle@users.noreply.github.com>
2023-01-26 13:42:15 -0500

    Add files via upload

--- Test 1/API_Test_1.py
+++ Test 1/API_Test_1.py
@@ -1,38 +1,11 @@
 import requests 
 import json
 
-url = "https://api.opendota.com/api/matches/6984621064"
-input_game_id = 6984621064
+#Used to test how to display usernames without all the other data
 
-def jprint(obj):
-    #creates a formatted sting of the python JSON object
-    text = json.loads()
-    print(text)
+url1 = f'https://api.opendota.com/api/players/163881259'
+r3 = requests.get(url1)
+data3 = json.loads(r3.text)
 
-def getGameID(input_game_id):
-    game = requests.get(f"https://api.opendota.com/api/matches/{input_game_id}").json()
-    print(game)
-    if "error" in game.keys():
-        return "ERROR"  
-    elif len(game["data"]) == 0:
-        return "ERROR"
-    else:
-        return game["data"][0]["id"]
-
-
-
-#Test run (working)
-
-
-match_response = requests.get(url)
-
-print(match_response.status_code)
-
-match_data = match_response.json()
-
-print(type(match_data))
-print(len(match_data))
-print(list(match_data.keys()))
-print("")
-
-
+print(data3['profile']['personaname'])
+print(json.dumps(data3))
--- Test 1/Match_Details.py
+++ Test 1/Match_Details.py
@@ -2,15 +2,42 @@
 
 import json
 import requests
+import time
+from random import randint
 
-url = "https://api.opendota.com/api/players/883661889/recentMatches"
+# Steam ID's for testing purposes
+# 883661889
+# 80037375
+# 163881259
 
-r = requests.get(url)
-data = json.loads(r.text)
+steam_id = input('please enter player steam id: ')
+steam_id = int(steam_id)
 
+#This gets the imformation about the player itself, no game data
+url1 = f'https://api.opendota.com/api/players/{steam_id}'
+r1 = requests.get(url1)
+data1 = json.loads("[" + r1.text + "]") #added brackets to treat it as an array
+
+#this gets all game data
+url2 = f'https://api.opendota.com/api/players/{steam_id}/recentMatches'
+r2 = requests.get(url2)
+data2 = json.loads(r2.text)
+
+#user_list is a array of 1 object, allows for easy display of player profile
+user_list = []
+for user in data1:
+    profile_name = user['profile']['personaname']
+    profile_pfp = user['profile']['avatarfull']
+
+    user_info = {
+        'username': profile_name,
+        'pfp': profile_pfp,
+    }
+    user_list.append(user_info)
+
+#array of a collection of games, stats vary depending on what is going to be shown
 game_list = []
-
-for game in data:
+for game in data2:
     match_id = game['match_id']
     duration = game['duration']
     hero_id = game['hero_id']
@@ -21,23 +48,24 @@ for game in data:
     
     match_info = {
         'match_id': match_id,
-        'duration': duration,
+        'duration': time.strftime("%M:%S", time.gmtime(duration)), #converts the time from seconds to minutes (and seconds) in standard format
         'hero_id': hero_id,
         'kills': kills,
         'deaths': deaths,
         'assists': assists,
-        'party_size': party_size
-        
-        
+        'party_size': party_size     
     }
     game_list.append(match_info)
-    
-    
 
-print("XXXXX's Recent Matches")
-print(game_list)
+print(user_list)
+print(game_list[0])
+print(game_list[1])
 
 
+"""  This is code to get a random game out of the list
+value = randint(0, len(game_list))    
+try_this = game_list[value]
+""" 
 """ 
 person_list = []
 
