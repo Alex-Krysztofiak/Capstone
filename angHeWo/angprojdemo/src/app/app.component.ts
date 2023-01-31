71ee445e5af3305a2fb538d37045515cde8ff874
TDZnZ <114675010+TDZnZ@users.noreply.github.com>
2023-01-30 19:13:12 -0500

    Mockup page

---
+++ angHeWo/angprojdemo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./homepage_style.css']
+})
+export class AppComponent {
+  title = 'angprojdemo';
+}
